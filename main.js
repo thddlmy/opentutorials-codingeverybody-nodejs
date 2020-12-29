d913e0013d408320cc296a3b758aed84f4ef36c3
thddlmy <thddlmy@gmail.com>
2020-12-30 00:00:54 +0900

    2020-12-30 commit

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ var fs = require('fs');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var title= queryData.id;
     console.log(queryData.id);
     if(_url == '/'){
-      _url = '/index.html';
+      title = 'Welcome'
     }
     if(_url == '/favicon.ico'){
       //return response.writeHead(404);
@@ -15,7 +16,27 @@ var app = http.createServer(function(request,response){
       return;
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + _url));
- 
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+      var template = `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p>${description}</p>
+    </body>
+    </html>
+    `
+    response.end(template);
+    });   
 });
 app.listen(3001);
